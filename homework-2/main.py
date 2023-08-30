de097fd5472dfef07476805d40da559f9b54f472
pavel <Dante94_94@bk.ru>
2023-08-30 22:06:24 +0300

    1. Add absolute path in instantiate_from_csv. 2. Delete return in setter.

--- homework-2/main.py
+++ homework-2/main.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
     Item.instantiate_from_csv('../src/items.csv')  # создание объектов из данных файла
     assert len(Item.all) == 6  # в файле 5 записей с данными по товарам
 
-    # item1 = Item.all[0]
-    #
-    # print(item1.name)
-    # assert item1.name == 'Смартфон'
+    item1 = Item.all[1]
+
+    print(item1.name)
+    assert item1.name == 'Смартфон'
 
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
